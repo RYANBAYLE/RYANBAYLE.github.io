a0b24744992db2d6971c205d06b1b9559c13c5e5
Ryan <ryanbayle.24@gmail.com>
2024-12-28 04:08:17 +0100

    28/12/2024

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ const vertMachine = document.getElementById("vertMachine");
 const rouge = document.getElementById("rouge");
 const bleu = document.getElementById("bleu");
 const vert = document.getElementById("vert");
-const finish = document.getElementById("finish"); // Sélection de l'écran de fin
+const win = document.getElementById("win"); // Sélection de l'écran de fin
+const loose = document.getElementById("loose"); // Sélection de l'écran de fin
 const resetButton = document.getElementById("rejouer");
 const scoreCounter = document.getElementById("scoreCounter");
 const machineColors = [rougeMachine, bleuMachine, vertMachine];
 const countdownElement = document.getElementById("countdown");
 let score = 0;
+
+// save classement
+
+function storeMorpion() {
+  window.localStorage.morpionsList += palmarèsList.innerHTML;
+}
+function saveMorpionList() {
+  if (window.localStorage.morpionsList) {
+    palmarèsList.innerHTML += window.localStorage.morpionsList;
+  }
+}
 
 P1.addEventListener("click", (e) => {
   // Trouver la div parente avec l'ID (si elle existe)
@@ -90,9 +102,22 @@ P1.addEventListener("click", (e) => {
     }
 
     // Vérifier les conditions de fin de jeu
-    if (score === 3 || score === -3) {
-      finish.style.visibility = "visible";
-      finish.style.opacity = "1";
+    if (score === 2) {
+      win.style.visibility = "visible";
+      win.style.opacity = "1";
+      P1.style.pointerEvents = "none"; // Désactiver les clics
+      const pseudo = playerName.value.trim();
+      if (pseudo) {
+        let date = new Date();
+        let dateFormat = date.toLocaleDateString("fr-FR");
+        palmarèsList.innerHTML += `<li>${pseudo} ---------------------------------- ${dateFormat}</li>`;
+        storeMorpion();
+      }
+    }
+
+    if (score === -1) {
+      loose.style.visibility = "visible";
+      loose.style.opacity = "1";
       P1.style.pointerEvents = "none"; // Désactiver les clics
     }
   } else {
@@ -115,8 +140,64 @@ resetButton.addEventListener("click", () => {
   rouge.style.opacity = "1";
   bleu.style.opacity = "1";
   vert.style.opacity = "1";
-  finish.style.visibility = "hidden"; // Masquer l'écran de fin
-  finish.style.opacity = "0";
+  win.style.visibility = "hidden"; // Masquer l'écran de fin
+  win.style.opacity = "0";
+  loose.style.visibility = "hidden"; // Masquer l'écran de fin
+  loose.style.opacity = "0";
   P1.style.pointerEvents = "auto"; // Réactiver les clics
   countdownElement.style.opacity = "0";
 });
+
+// Changement de couleur sur les lettres du titre
+const nom = document.querySelector(".Nom");
+
+nom.innerHTML = nom.innerHTML.replace("T", '<span class="T">T</span>');
+nom.innerHTML = nom.innerHTML.replace("M", '<span class="M">M</span>');
+nom.innerHTML = nom.innerHTML.replace("G", '<span class="G">G</span>');
+
+// nom du player
+const playerCard = document.querySelector(".Player-card");
+const nameEdited = document.getElementById("name-edited");
+const playerName = document.getElementById("player-name");
+const changerNom = document.getElementById("changer-nom");
+
+playerCard.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const pseudo = playerName.value.trim();
+  let date = new Date();
+  let dateFormat = date.toLocaleDateString("fr-FR");
+  let Morpion = {
+    pseudo: pseudo,
+    when: dateFormat,
+  };
+  nameEdited.textContent = "Morpion: " + pseudo;
+  nameEdited.style.textAlign = "center";
+  playerName.style.display = "none";
+  changerNom.style.display = "inline-block";
+});
+
+changerNom.addEventListener("click", () => {
+  playerName.value = "";
+
+  playerName.style.display = "inline-block";
+  changerNom.style.display = "none";
+});
+
+// Palmarès des morpions
+const palmarèsMorpions = document.querySelector(".palmarès-morpions");
+const palmarèsButton = document.getElementById("palmarès");
+const palmarèsWindow = document.querySelector(".palmarès-window");
+const palmarèsList = document.getElementById("palmarès-list");
+
+palmarèsButton.addEventListener("click", () => {
+  if (palmarèsWindow.style.visibility === "hidden") {
+    palmarèsWindow.style.visibility = "visible";
+    palmarèsWindow.style.opacity = "1";
+  } else {
+    palmarèsWindow.style.visibility = "hidden";
+    palmarèsWindow.style.opacity = "0";
+  }
+});
+
+saveMorpionList();
